20e0cab6c46a29720b04bf9ed0e57fc8285a607c
sumitsharma05 <sumitsharmabca05@gmail.com>
2026-02-06 18:22:37 +0530

    Initial commit - NeonAuth animated login UI

---
+++ script1.js
@@ -0,0 +1,42 @@
+const container= document.querySelector('.container');
+const inputs= document.querySelectorAll('input');
+const CreateBtn= document.querySelector('#CreateBtn');
+const loginBtn = document.getElementById('loginBtn');
+let hue =0;
+
+function animateBorder(){
+    hue=(hue+1)%360;
+    const nextHue=(hue+60)%360;
+     container.style.borderImage = `linear-gradient(45deg, hsl(${hue},100%,50%), hsl(${nextHue},100%,50%)) 1`;
+    requestAnimationFrame(animateBorder);
+
+}
+
+animateBorder();
+
+
+loginBtn.addEventListener("click",()=>{
+    window.location.href="index.html";
+
+});
+
+
+
+
+
+// inputs.forEach(input=>{
+//     input.addeventlistner('focus',()=>{
+//         container.style.boxShadow = '0 0 30px 5px cyan';
+//     });
+//     input.addeventlistner('blur',()=>{
+//         container.style.boxShadow = '0 0 10px 2px purple';
+//     });
+
+// });
+
+
+// loginBtn.addEventListener('click',()=>{
+//      const randomHue = Math.floor(Math.random() * 360);
+//   container.style.borderImage = `linear-gradient(45deg, hsl(${randomHue},100%,50%), hsl(${(randomHue+60)%360},100%,50%)) 1`;
+//   alert('Login button clicked!'); 
+// })
