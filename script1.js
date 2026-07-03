73a2dbe64b0b5ef5e3b2d00923f2d2a86aff6535
sumit sharma <sumitsharmabca05@gmail.com>
2026-07-03 20:51:11 +0530

    Clean code
    Removed commented-out code for input event listeners and login button.

--- script1.js
+++ script1.js
@@ -20,23 +20,3 @@ loginBtn.addEventListener("click",()=>{
 
 });
 
-
-
-
-
-// inputs.forEach(input=>{
-//     input.addeventlistner('focus',()=>{
-//         container.style.boxShadow = '0 0 30px 5px cyan';
-//     });
-//     input.addeventlistner('blur',()=>{
-//         container.style.boxShadow = '0 0 10px 2px purple';
-//     });
-
-// });
-
-
-// loginBtn.addEventListener('click',()=>{
-//      const randomHue = Math.floor(Math.random() * 360);
-//   container.style.borderImage = `linear-gradient(45deg, hsl(${randomHue},100%,50%), hsl(${(randomHue+60)%360},100%,50%)) 1`;
-//   alert('Login button clicked!'); 
-// })
